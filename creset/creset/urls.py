a8ec094cbbe2ff9776110fcb67ff2c2ef91e6cfc
krisornkachai <krisornkachai@gmail.com>
2019-10-31 00:05:26 +0700

    combine vue and django

--- creset/creset/urls.py
+++ creset/creset/urls.py
@@ -1,4 +1,4 @@
-"""creset URL Configuration
+"""senior_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -12,10 +12,16 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    sss
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include,url
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
+  
+    
 ]
