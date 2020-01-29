1dadc5e5f3ef773df86c53c03d99591080864801
krisornkachai <krisornkachai@gmail.com>
2020-01-30 02:37:35 +0700

    commit1

--- creset/creset/urls.py
+++ creset/creset/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
     sss
 """
-from django.contrib import admin
+'''from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include,url
 from django.views.generic import TemplateView
@@ -25,3 +25,25 @@ urlpatterns = [
   
     
 ]
+'''
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.contrib.auth.views import PasswordResetView, LogoutView
+from server.views import LoginView
+
+
+urlpatterns = [
+    path('', include('authentification.urls')),
+    path('', include('server.urls')),
+    path('admin/', admin.site.urls),
+    path('social/', include('social_django.urls')),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('api-auth/', include('rest_framework.urls')),
+    path('v1/', include('api.urls')),
+]
+
+if 'cloud_browser' in settings.INSTALLED_APPS:
+    urlpatterns.append(path('cloud-storage/', include('cloud_browser.urls')))
